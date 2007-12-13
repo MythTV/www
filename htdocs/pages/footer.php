41d40ba78a8e353e721168f050f86883b827e038
Chris Petersen <cpetersen@mythtv.org>
2007-12-13 07:02:35 +0000

    lots of little improvements in overall lookfeel
    git-svn-id: file:///usr/src/myth/mythwww/www.mythtv.org@76 474a20e3-f028-0410-92a6-ca0ac9e30e78

--- htdocs/pages/footer.php
+++ htdocs/pages/footer.php
@@ -2,14 +2,14 @@
 
 <!-- Footer region and sponsorship text -->
 <div id="footer">
-<span id="copyright">
-    &copy; 2002-<?php echo date('Y') ?> by Isaac Richards and the MythTV Developers
-</span>
-<span id="sponsorship">
-Sponsored by:
-<a href="http://www.osuosl.org/"><img src="/img/osl.gif" alt="OSU OSL"></a>
-<a href="http://www.siliconmechanics.com/"><img src="/img/simech.gif" alt="Silicon Mechanics"></a>
-</span>
+    <span id="copyright">
+        &copy; 2002-<?php echo date('Y') ?> by Isaac Richards and the MythTV Developers
+    </span>
+    <span id="sponsorship">
+    Sponsored by:
+    <a href="http://www.osuosl.org/"><img src="/img/osl.gif" alt="OSU OSL"></a>
+    <a href="http://www.siliconmechanics.com/"><img src="/img/simech.gif" alt="Silicon Mechanics"></a>
+    </span>
 </div>
 
 <!-- End of the page -->
--- htdocs/tmpl/news.php
+++ htdocs/tmpl/news.php
@@ -1,10 +1,10 @@
 <div class="news_post">
-    <h3><?php echo $title ?></h3>
-    <div class="post_content">
-        <?php echo $news ?>
-    </div>
     <div class="post_author">
         Posted by <?php echo $author ?>
         on <?php echo date('l, F j @ G:i T', $date); ?>
+    </div>
+    <h3><?php echo $title ?></h3>
+    <div class="post_content">
+        <?php echo $news ?>
     </div>
 </div>
