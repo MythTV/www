e024498743cb78b6e4ded6762664e86683bda72f
Chris Petersen <cpetersen@mythtv.org>
2007-12-31 02:11:34 +0000

    Add section headers for the various plugins.
    git-svn-id: file:///usr/src/myth/mythwww/www.mythtv.org@82 474a20e3-f028-0410-92a6-ca0ac9e30e78

--- htdocs/pages/detail.php
+++ htdocs/pages/detail.php
@@ -22,12 +22,72 @@
 
 Detail article (containing screenshots) coming soon.
 
-<h3>MythWeb</h3>
+<h2>MythTV</h2>
+
+Something about MythTV itself -- backend/frontend, distributed, etc
+
+<h2>MythMusic</h2>
+
+This plugin lets you play audio files or CDs from your music collection,
+complete with visualizers.  It will also play music from your iTunes
+shared library.
+
+<h2>MythVideo</h2>
+
+Video/DVD playback....
+
+<h2>MythArchive</h2>
+
+Archive your recorded programs to DVD...
+
+<h2>MythBrowser</h2>
+
+Web browser based on the KHTML rendering engine.
+
+<h2>MythControls</h2>
+
+What does this do?  Configurable key control editor?
+
+<h2>MythFlix</h2>
+
+Browse your netflix account and order movies.
+
+<h2>MythGallery</h2>
+
+Browse through and show off your photo collection.
+
+<h2>MythGame</h2>
+
+Integration with MAME, xNES and other emulators to let you play video
+games from with MythTV
+
+<h2>MythMovies</h2>
+
+Local Movie times?
+
+<h2>MythNews</h2>
+
+RSS News feed plugin.  Requires MythBrowser?
+
+<h2>MythPhone</h2>
+
+Integrate with Asterix
+
+<h2>MythWeather</h2>
+
+Local weather updates.  Does this still have bugs in the US?
+
+<h2>MythZoneMinder</h2>
+
+Link up to your ZoneMinder video surveilance system and browse all of
+your cameras from the comfort of your couch.
+
+<h2>MythWeb</h2>
 
 <p>
 <div style="float: right; margin-left: 2em">
-<?php tv_thumb('mythweb_listing.png', 'tv_black', 'r', 'a')  ?>
-<?php tv_thumb('mythweb_details.png', 'tv_black', 'l', 'a')  ?>
+<?php tv_thumb('mythweb_listing.png', 'tv_grey', 'r', 'a')  ?>
+<?php tv_thumb('mythweb_details.png', 'tv_grey', 'l', 'a')  ?>
 </div>
 So this is the place where I would talk about MythWeb...
 Show a few screenshots...
@@ -45,8 +105,8 @@ Show a few screenshots...
 
 <p style="clear: right">
 <div style="float: left; margin-right: 2em">
-<?php tv_thumb('mythweb_upcoming.png', 'tv_white', 'r', 'a')  ?>
-<?php tv_thumb('mythweb_recorded.png', 'tv_white', 'l', 'a')  ?>
+<?php tv_thumb('mythweb_upcoming.png', 'tv_grey', 'r', 'a')  ?>
+<?php tv_thumb('mythweb_recorded.png', 'tv_grey', 'l', 'a')  ?>
 </div>
 Maybe talk some more, and show some more screenshots...
 Maybe talk some more, and show some more screenshots...
