064883784b5e1d495476045610db6fd07f69ceb3
Chris Petersen <cpetersen@mythtv.org>
2008-11-08 19:23:46 +0000

    update news items.
    git-svn-id: file:///usr/src/myth/mythwww/www.mythtv.org@97 474a20e3-f028-0410-92a6-ca0ac9e30e78

---
+++ htdocs/news/127.php
@@ -0,0 +1,34 @@
+<?php
+    $title  = 'MythTV 0.21';
+    $author = '<a href="http://www.mythtv.org/">Isaac</a>';
+    $date   = strtotime('2008-03-08 22:53:51 UTC ');
+?>
+<ul>
+    <li>New release!  It's been quite a while, so there's lots of new stuff.
+        Notably: autodiscovery (less manual configuration of new frontends),
+        storage groups (no need for LVM/etc), support for multiple recordings
+        on one DVB/ATSC multiplex, a couple new plugins, some new
+        deinterlacing/video display options, and many, many other things.
+        </li>
+    <li>The <a href="http://www.mythtv.org/wiki/index.php/Release_Notes_-_0.21">Release
+        notes page in the wiki</a> has the list of what's changed, but it's
+        currently a couple thousand checkins out of date.
+        </li>
+    <li>Standard release info:
+        <ul>
+            <li>Grab the release from the
+                <a href="http://www.mythtv.org/modules.php?name=Downloads">download
+                section</a> and please at least try to read the
+                <a href="http://www.mythtv.org/modules.php?name=MythInstall">docs</a>
+                before asking questions.
+                </li>
+            <li>The <a href="http://www.mythtv.org/modules.php?name=Web_Links&amp;l_op=viewlink&amp;cid=1">binary
+                packages</a> should hopefully be updated to 0.21 soon.</li>
+            <li>See this
+                <a href="http://www.mythtv.org/modules.php?name=Web_Links&amp;l_op=viewlink&amp;cid=3">links
+                section</a> for a list of linux distributions based around MythTV.
+                </li>
+        </ul>
+        </li>
+<ul>
+</ul>
