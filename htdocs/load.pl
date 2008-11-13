d12a9efd574e81c2ba2b24bd8c1dba72c330a9eb
Chris Petersen <cpetersen@mythtv.org>
2008-11-13 07:56:08 +0000

    add support for topic names
    git-svn-id: file:///usr/src/myth/mythwww/www.mythtv.org@107 474a20e3-f028-0410-92a6-ca0ac9e30e78

--- htdocs/load.pl
+++ htdocs/load.pl
@@ -1,6 +1,19 @@
 #!/usr/bin/perl -w
 #
 
+# Topics (lifted from the nuke_topics table)
+    my %topics = (
+       2 => 'news',          # mythnews
+       3 => 'news',          # hardware
+       4 => 'announcement',  # announce
+       5 => 'bug',           # bugs
+       6 => 'news',          # codehacks
+       7 => 'news',          # ideas
+       8 => 'news',          # links
+       9 => 'news',          # questions
+      10 => 'software_news', # software
+      11 => 'theme_news',    # themes
+    );
 
 # Connect
     use DBI;
@@ -10,9 +23,8 @@
         or die "Cannot connect to database: $!\n\n";
 
 # Extract
-    my $sh = $dbh->prepare('SELECT sid, title, time, hometext FROM nuke_stories');
+    my $sh = $dbh->prepare('SELECT sid, topic, title, time, hometext FROM nuke_stories');
     $sh->execute();
-
 
 # Build the list
     while (my $row = $sh->fetchrow_hashref) {
@@ -20,6 +32,7 @@
         open(DATA, ">news/$row->{sid}.php") or die "can't write news/$row->{sid}.php:  $!\n";
         print DATA <<EOF;
 <?php
+    \$topic  = '$topics{$row->{topic}}';
     \$title  = '$row->{title}';
     \$author = '<a href="http://www.mythtv.org/">Isaac</a>';
     \$date   = strtotime('$row->{time} UTC');
