b900c725e4b01d588a9d7059291f4d49b5a01f62
Chris Petersen <cpetersen@mythtv.org>
2008-11-16 05:02:20 +0000

    Reorganize detail page so it's semi-useful.
    git-svn-id: file:///usr/src/myth/mythwww/www.mythtv.org@125 474a20e3-f028-0410-92a6-ca0ac9e30e78

--- htdocs/pages/detail.php
+++ htdocs/pages/detail.php
@@ -19,159 +19,200 @@
     require_once 'tmpl/header.php';
 ?>
 
-<div id="detail">
-
-<h1>MythTV In Detail</h1>
-
-<div id="detail_toc">
-    <ul>
-        <li><a href="#mythtv">MythTV</a></li>
-        <li><a href="#mythmusic">MythMusic</a></li>
-        <li><a href="#mythvideo">MythVideo</a></li>
-        <li><a href="#mytharchive">MythArchive</a></li>
-        <li><a href="#mythbrowser">MythBrowser</a></li>
-        <li><a href="#mythcontrols">MythControls</a></li>
-        <li><a href="#mythflix">MythFlix</a></li>
-        <li><a href="#mythgallery">MythGallery</a></li>
-        <li><a href="#mythgame">MythGame</a></li>
-        <li><a href="#mythmovies">MythMovies</a></li>
-        <li><a href="#mythnews">MythNews</a></li>
-        <li><a href="#mythphone">MythPhone</a></li>
-        <li><a href="#mythweather">MythWeather</a></li>
-        <li><a href="#mythzoneminder">MythZoneminder</a></li>
-        <li><a href="#mythweb">MythWeb</a></li>
-    </ul>
-</div><!-- #detail_toc -->
-
-<div id="detail_content">
-
-    <div class="content" id="detail_mythtv">
-
-        <a name="mythtv"></a>
-        <h2>MythTV</h2>
-
-        Something about MythTV itself -- backend/frontend, distributed, etc
-
+<div id="detail" class="inset">
+    <div class="left">
+        <div class="top"></div>
+        <div class="middle">
+            <div class="title"></div>
+        </div>
+        <div class="bottom"></div>
     </div>
+    <div class="content">
+        <div id="detail-toc">
+            <h1>MythTV In Detail</h1>
+            <ul>
+                <li><a href="#mythtv">MythTV</a></li>
+                <li><a href="#mytharchive">MythArchive</a></li>
+                <li><a href="#mythbrowser">MythBrowser</a></li>
+                <li><a href="#mythcontrols">MythControls</a></li>
+                <li><a href="#mythflix">MythFlix</a></li>
+                <li><a href="#mythgallery">MythGallery</a></li>
+                <li><a href="#mythgame">MythGame</a></li>
+                <li><a href="#mythmovies">MythMovies</a></li>
+                <li><a href="#mythmusic">MythMusic</a></li>
+                <li><a href="#mythnews">MythNews</a></li>
+                <li><a href="#mythphone">MythPhone</a></li>
+                <li><a href="#mythvideo">MythVideo</a></li>
+                <li><a href="#mythweather">MythWeather</a></li>
+                <li><a href="#mythweb">MythWeb</a></li>
+                <li><a href="#mythzoneminder">MythZoneminder</a></li>
+            </ul>
+        </div><!-- #detail-toc -->
+        <div id="detail-content">
 
-    <div class="content" id="detail_mythmusic">
+        <div id="detail-mythtv" class="detail">
+            <a name="mythtv"></a>
+            <h2>MythTV</h2>
 
-        <a name="mythmusic"></a>
-        <h2>MythMusic</h2>
+            Something about MythTV itself -- backend/frontend, distributed, etc
 
-        This plugin lets you play audio files or CDs from your music collection,
-        complete with visualizers.  It will also play music from your iTunes
-        shared library.
+        </div>
 
-    </div>
+        <div id="detail-mythmusic" class="detail">
+            <a name="mythmusic"></a>
+            <h2>MythMusic</h2>
 
-<?php /*
-    <h2>MythVideo</h2>
+            This plugin lets you play audio files or CDs from your music collection,
+            complete with visualizers.  It will also play music from your iTunes
+            shared library.
 
-    Video/DVD playback....
+        </div>
 
-    <h2>MythArchive</h2>
+        <div id="detail-mythvideo" class="detail">
+            <a name="mythvideo"></a>
+            <h2>MythVideo</h2>
 
-    Archive your recorded programs to DVD...
+            Video/DVD playback....
+        </div>
 
-    <h2>MythBrowser</h2>
+        <div id="detail-mytharchive" class="detail">
+            <a name="mytharchive"></a>
+            <h2>MythArchive</h2>
 
-    Web browser based on the KHTML rendering engine.
+            Archive your recorded programs to DVD...
+        </div>
 
-    <h2>MythControls</h2>
+        <div id="detail-mythbrowser" class="detail">
+            <a name="mythbrowser"></a>
+            <h2>MythBrowser</h2>
 
-    What does this do?  Configurable key control editor?
+            Web browser based on the KHTML rendering engine.
+        </div>
 
-    <h2>MythFlix</h2>
+        <div id="detail-mythcontrols" class="detail">
+            <a name="mythcontrols"></a>
+            <h2>MythControls</h2>
 
-    Browse your netflix account and order movies.
+            What does this do?  Configurable key control editor?
+        </div>
 
-    <h2>MythGallery</h2>
+        <div id="detail-mythflix" class="detail">
+            <a name="mythflix"></a>
+            <h2>MythFlix</h2>
 
-    Browse through and show off your photo collection.
+            Browse your netflix account and order movies.
+        </div>
 
-    <h2>MythGame</h2>
+        <div id="detail-mythgallery" class="detail">
+            <a name="mythgallery"></a>
+            <h2>MythGallery</h2>
 
-    Integration with MAME, xNES and other emulators to let you play video
-    games from with MythTV
+            Browse through and show off your photo collection.
+        </div>
 
-    <h2>MythMovies</h2>
+        <div id="detail-mythgame" class="detail">
+            <a name="mythgame"></a>
+            <h2>MythGame</h2>
 
-    Local Movie times?
+            Integration with MAME, xNES and other emulators to let you play video
+            games from with MythTV
+        </div>
 
-    <h2>MythNews</h2>
+        <div id="detail-mythmovies" class="detail">
+            <a name="mythmovies"></a>
+            <h2>MythMovies</h2>
 
-    RSS News feed plugin.  Requires MythBrowser?
+            Local Movie times?
+        </div>
 
-    <h2>MythPhone</h2>
+        <div id="detail-mythnews" class="detail">
+            <a name="mythnews"></a>
+            <h2>MythNews</h2>
 
-    Integrate with Asterix
+            RSS News feed plugin.  Requires MythBrowser?
+        </div>
 
-    <h2>MythWeather</h2>
+        <div id="detail-mythphone" class="detail">
+            <a name="mythphone"></a>
+            <h2>MythPhone</h2>
 
-    Local weather updates.  Does this still have bugs in the US?
+            Integrate with Asterix
+        </div>
 
-    <h2>MythZoneMinder</h2>
+        <div id="detail-mythweather" class="detail">
+            <a name="mythweather"></a>
+            <h2>MythWeather</h2>
 
-    Link up to your ZoneMinder video surveilance system and browse all of
-    your cameras from the comfort of your couch.
+            Local weather updates.  Does this still have bugs in the US?
+        </div>
 
-    <h2>MythWeb</h2>
+        <div id="detail-mythzoneminder" class="detail">
+            <a name="mythzoneminder"></a>
+            <h2>MythZoneMinder</h2>
 
-    <p>
-    <div style="float: right; margin-left: 2em">
-    <?php tv_thumb('mythweb_listing.png', 'tv_grey', 'r', 'a', '#88a1bb')  ?>
-    <?php tv_thumb('mythweb_details.png', 'tv_grey', 'l', 'a', '#88a1bb')  ?>
-    </div>
-    So this is the place where I would talk about MythWeb...
-    Show a few screenshots...
-    So this is the place where I would talk about MythWeb...
-    Show a few screenshots...
-    So this is the place where I would talk about MythWeb...
-    Show a few screenshots...
-    So this is the place where I would talk about MythWeb...
-    Show a few screenshots...
-    So this is the place where I would talk about MythWeb...
-    Show a few screenshots...
-    So this is the place where I would talk about MythWeb...
-    Show a few screenshots...
-    </p>
+            Link up to your ZoneMinder video surveilance system and browse all of
+            your cameras from the comfort of your couch.
+        </div>
 
-    <p style="clear: right">
-    <div style="float: left; margin-right: 2em">
-    <?php tv_thumb('mythweb_upcoming.png', 'tv_grey', 'r', 'a', '#88a1bb')  ?>
-    <?php tv_thumb('mythweb_recorded.png', 'tv_grey', 'l', 'a', '#88a1bb')  ?>
-    </div>
-    Maybe talk some more, and show some more screenshots...
-    Maybe talk some more, and show some more screenshots...
-    Maybe talk some more, and show some more screenshots...
-    Maybe talk some more, and show some more screenshots...
-    Maybe talk some more, and show some more screenshots...
-    Maybe talk some more, and show some more screenshots...
-    Maybe talk some more, and show some more screenshots...
-    </p>
+        <div id="detail-mythweb" class="detail">
+            <a name="mythweb"></a>
+            <h2>MythWeb</h2>
 
-    <p>
-    <div style="float: right; margin-left: 2em">
-    <?php tv_thumb('mythweb_video.png', 'tv_grey', 'r', 'a', '#88a1bb')  ?>
-    <?php tv_thumb('mythweb_recdetails.png', 'tv_grey', 'l', 'a', '#88a1bb')  ?>
-    </div>
-    So this is the place where I would talk about MythWeb...
-    Show a few screenshots...
-    So this is the place where I would talk about MythWeb...
-    Show a few screenshots...
-    So this is the place where I would talk about MythWeb...
-    Show a few screenshots...
-    So this is the place where I would talk about MythWeb...
-    Show a few screenshots...
-    So this is the place where I would talk about MythWeb...
-    Show a few screenshots...
-    So this is the place where I would talk about MythWeb...
-    Show a few screenshots...
-    </p>
-*/ ?>
-</div><!-- #detail_content -->
+            <p>
+            <div style="float: right; margin-left: 2em">
+            <?php tv_thumb('mythweb_listing.png', 'tv_black', 'l', 'a', '#e6e6f0')  ?>
+            <?php tv_thumb('mythweb_details.png', 'tv_black', 'r', 'a', '#e6e6f0')  ?>
+            </div>
+            So this is the place where I would talk about MythWeb...
+            Show a few screenshots...
+            So this is the place where I would talk about MythWeb...
+            Show a few screenshots...
+            So this is the place where I would talk about MythWeb...
+            Show a few screenshots...
+            So this is the place where I would talk about MythWeb...
+            Show a few screenshots...
+            So this is the place where I would talk about MythWeb...
+            Show a few screenshots...
+            So this is the place where I would talk about MythWeb...
+            Show a few screenshots...
+            </p>
 
+            <p style="clear: right">
+            <div style="float: left; margin-right: 2em">
+            <?php tv_thumb('mythweb_upcoming.png', 'tv_black', 'l', 'a', '#e6e6f0')  ?>
+            <?php tv_thumb('mythweb_recorded.png', 'tv_black', 'r', 'a', '#e6e6f0')  ?>
+            </div>
+            Maybe talk some more, and show some more screenshots...
+            Maybe talk some more, and show some more screenshots...
+            Maybe talk some more, and show some more screenshots...
+            Maybe talk some more, and show some more screenshots...
+            Maybe talk some more, and show some more screenshots...
+            Maybe talk some more, and show some more screenshots...
+            Maybe talk some more, and show some more screenshots...
+            </p>
+
+            <p>
+            <div style="float: right; margin-left: 2em">
+            <?php tv_thumb('mythweb_video.png',      'tv_black', 'l', 'a', '#e6e6f0')  ?>
+            <?php tv_thumb('mythweb_recdetails.png', 'tv_black', 'r', 'a', '#e6e6f0')  ?>
+            </div>
+            So this is the place where I would talk about MythWeb...
+            Show a few screenshots...
+            So this is the place where I would talk about MythWeb...
+            Show a few screenshots...
+            So this is the place where I would talk about MythWeb...
+            Show a few screenshots...
+            So this is the place where I would talk about MythWeb...
+            Show a few screenshots...
+            So this is the place where I would talk about MythWeb...
+            Show a few screenshots...
+            So this is the place where I would talk about MythWeb...
+            Show a few screenshots...
+            </p>
+        </div>
+
+        </div><!-- #detail-content -->
+    </div><!-- class:content -->
 </div><!-- #detail -->
 
 <?php
