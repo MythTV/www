a55dea5e7e0123ca1ab49fd38d78e2a8891a1716
Chris Petersen <cpetersen@mythtv.org>
2009-01-16 03:24:23 +0000

    add new website announcement news item
    git-svn-id: file:///usr/src/myth/mythwww/www.mythtv.org@173 474a20e3-f028-0410-92a6-ca0ac9e30e78

---
+++ htdocs/news/128.php
@@ -0,0 +1,32 @@
+<?php
+    $topic  = 'announcement';
+    $title  = 'New Website!';
+    $author = 'xris';
+    $date   = strtotime('2009-01-16 2:00:00 UTC');
+?>
+<ul>
+    <li>After many years running under PHP-Nuke, MythTV.org is now proudly
+        running its own customized website application, which we hope will be
+        faster and more secure than the old site.  Plus, we think it looks a
+        <strong>whole lot</strong> better than the old site.
+        </li>
+    <li>The new website has a lot of new features including an RSS/Atom feed,
+        the digg links to the right, and an all new &quot;MythTV in Detail&quot;
+        section, so please feel free to poke around and explore the new layout.
+        It's also a lot easier for us to maintain, update, and improve, so we
+        hope to be able to provide more frequent updates than before, too.
+        </li>
+    <li>If you get a chance, drop by #mythtv-users on irc.freenode.net and
+        thank designer <a href="http://miffteevee.co.uk/">Stuart Morgan</a>
+        (gbee) and coder <a href="http://forevermore.net/">Chris Petersen</a>
+        (xris) for all of the great work they put into setting up the new site.
+        </li>
+    <li>On a side note, <a href="http://schedulesdirect.org/">Schedules Direct</a>
+        is looking for information about users from outside of the US and Canada.
+        We know that there are a lot of MythTV users abroad, and it would really
+        help them out if you would head over there and fill out the
+        <a href="http://xris.schedulesdirect.org/survey/where">survey</a> that
+        they've posted.
+        </li>
+</ul>
+
