77611c61be38a5a4ebd5c3d8d138e77f515f3af2
Chris Petersen <cpetersen@mythtv.org>
2009-01-16 04:25:38 +0000

    this is perl, not bash
    git-svn-id: file:///usr/src/myth/mythwww/www.mythtv.org@182 474a20e3-f028-0410-92a6-ca0ac9e30e78

--- publish.pl
+++ publish.pl
@@ -67,7 +67,7 @@
         or die "Can't install mythtv.conf.apache\n";
 
 # Make sure the files are all owned properly
-    chown -R www\:www $target
+    system('chown -R www\:www '.shell_escape($target));
 
 # Re-enable the website
     unlink "$target/site_is_disabled"
