ce49e72487648ae28025d8dcb00038c1bb4dd723
Chris Petersen <cpetersen@mythtv.org>
2009-06-19 02:46:09 +0000

    new news item
    git-svn-id: file:///usr/src/myth/mythwww/www.mythtv.org@254 474a20e3-f028-0410-92a6-ca0ac9e30e78

---
+++ news/129.php
@@ -0,0 +1,31 @@
+<?php
+    $topic  = 'news';
+    $title  = 'Update and a request to international MythTV users';
+    $author = 'xris';
+    $date   = strtotime('2009-06-18  4:55:54 UTC');
+?>
+<ul>
+    <li>Just a quick update to let you all know that development work on
+        <a href="http://www.mythtv.org/wiki/Release_Notes_-_0.22">0.22</a>
+        is still progressing smoothly.  We appreciate your patience while
+        we are working through some of the more tedious parts of this upcoming
+        release.</li>
+    <li>In other news, <a href="http://schedulesdirect.org/">Schedules Direct</a>
+        is still looking for input from users from outside of the US and
+        Canada.  We know that there are a lot of MythTV users abroad, and it
+        would really help them out if you would head over there and
+        <a href="http://xris.schedulesdirect.org/survey/where">fill out the survey</a>
+        that they've posted.
+        </li>
+    <li>Schedules Direct is also looking for
+        <a href="http://forums.schedulesdirect.org/viewtopic.php?f=3&t=710">volunteer developers</a>
+        to help design and implement a new application to deliver guide data
+        themselves (rather than rely on TMS servers), which they expect to
+        serve to 20,000 or more members daily over the next few years.  They
+        have been a big help to the MythTV community, and could really use some
+        help.  If you have experience with Perl, PHP, and/or MySQL, please take
+        a look at their
+        <a href="http://forums.schedulesdirect.org/viewtopic.php?f=3&t=710">forum post</a>
+        for information about applying to help.
+        </li>
+</ul>
