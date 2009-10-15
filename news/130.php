37d5f871d7a2a4916418ca4ca589a2294d6a7585
Chris Petersen <cpetersen@mythtv.org>
2009-10-15 00:07:26 +0000

    new post about 0.22rc1
    git-svn-id: file:///usr/src/myth/mythwww/www.mythtv.org@262 474a20e3-f028-0410-92a6-ca0ac9e30e78

---
+++ news/130.php
@@ -0,0 +1,29 @@
+<?php
+    $topic  = 'announcement';
+    $title  = '0.22 Release Candidate Available';
+    $author = 'xris';
+    $date   = strtotime('2009-10-14 22:47:41 UTC');
+?>
+<ul>
+    <li>After what seemed to many like much too long of a wait, we've
+        finally started working on a release schedule for 0.22 and would
+        like to announce the availability of 0.22 Release Candidate 1.
+        </li>
+    <li class="bold">Please make sure to read the
+        <a href="http://www.mythtv.org/wiki/Release_Notes_-_0.22">Release Notes</a>
+        before upgrading.</li>
+    <li>You can <a href="ftp://ftp.osuosl.org/pub/mythtv/">download the tarballs</a> now:
+        <ul>
+            <li><a href="ftp://ftp.osuosl.org/pub/mythtv/mythtv-0.22rc1.tar.bz2">MythTV</a>
+                (<a href="http://svn.mythtv.org/svn/trunk/checksums/mythtv-0.22rc1.md5sum">MD5</a>)
+                </li>
+            <li><a href="ftp://ftp.osuosl.org/pub/mythtv/mythplugins-0.22rc1.tar.bz2">MythPlugins</a>
+                (<a href="http://svn.mythtv.org/svn/trunk/checksums/mythplugins-0.22rc1.md5sum">MD5</a>)
+                </li>
+            <li><a href="ftp://ftp.osuosl.org/pub/mythtv/myththemes-0.22rc1.tar.bz2">MythThemes</a>
+                (<a href="http://svn.mythtv.org/svn/trunk/checksums/myththemes-0.22rc1.md5sum">MD5</a>)
+                </li>
+        </ul></li>
+    <li>Or check the code out from
+        <a href="http://svn.mythtv.org/">subversion</a>.</li>
+</ul>
