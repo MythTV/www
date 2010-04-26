66c743a3278db4f56d9f70aa714c9dc53a4a50d6
Robert McNamara <rmcnamara@mythtv.org>
2010-04-26 23:50:19 +0000

    Add Theme competition winner announcement.
    git-svn-id: file:///usr/src/myth/mythwww/www.mythtv.org@295 474a20e3-f028-0410-92a6-ca0ac9e30e78

---
+++ news/136.php
@@ -0,0 +1,20 @@
+<?php
+    $topic  = 'announcement';
+    $title  = 'MythTV Theming Competition Results';
+    $author = 'robertm';
+    $date   = strtotime('2010-04-26 21:07:18 UTC');
+?>
+<ul>
+    <li>During the MythTV .23 development cycle, we ran a theming competition to invite users to rethink MythTV's UI in radical and creative new ways.  The MythTV developers and public have spoken, and the following are our winners!
+        </li>
+        <ul>
+            <li><b>First Place:</b> <a href="http://www.mythtv.org/themes/Childish.tar.gz">Childish</a>, a 4:3 theme by Michael Lynch, designed with the author's children in mind.
+                </li>
+            <li><b>Second Place:</b> <a href="http://www.mythtv.org/themes/LCARS.tar.gz">LCARS</a>, a 16:9 theme by Gary Butters based on a science fiction computer interface.
+                </li>
+            <li><b>Third Place:</b> <a href="http://www.mythtv.org/themes/TransBlue.tar.gz">TransBlue</a>, a minimalist 16:10 theme by Reto Wechner.
+                </li>
+        </ul>
+     <li>Congratulations to all involved for their hard work and perseverence.  Childish will be included in the MythThemes repository starting with the .23 release.  The winners of the theme competition will receive tuner and high definition capture devices courtesy of Hauppauge, as well as listings subscriptions from Schedules Direct, and a T-Shirt.
+         </li>
+</ul>
