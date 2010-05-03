f4e03f9520a9c3f4c3a6e75aa95e8784852d86b6
Robert McNamara <rmcnamara@mythtv.org>
2010-05-03 16:48:17 +0000

    Add .23 RC3 announcement.
    git-svn-id: file:///usr/src/myth/mythwww/www.mythtv.org@296 474a20e3-f028-0410-92a6-ca0ac9e30e78

---
+++ news/137.php
@@ -0,0 +1,37 @@
+<?php
+    $topic  = 'announcement';
+    $title  = '0.23 Release Candidate 3 Available';
+    $author = 'robertm';
+    $date   = strtotime('2010-05-03 17:27:52 UTC');
+?>
+<ul>
+    <li>The third Release Candidate for MythTV 0.23 is now available.
+        </li>
+    <li>Important changes since the last RC include:
+        <ul>
+            <li>Several crash fixes.
+                </li>
+            <li>Fix several bugs that prevented proper playback of DVDs and ISOs.
+                </li>
+            <li>Add the winner of the theming competition, Childish, to myththemes.
+                </li>
+        </ul></li>
+    <li class="bold">Please make sure to read the
+        <a href="http://www.mythtv.org/wiki/Release_Notes_-_0.23">Release Notes</a>
+        before upgrading.</li>
+    <li>You can <a href="ftp://ftp.osuosl.org/pub/mythtv/">download the tarballs</a> now:
+        <ul>
+            <li><a href="ftp://ftp.osuosl.org/pub/mythtv/mythtv-0.23-rc3.tar.bz2">MythTV</a>
+                (<a href="http://svn.mythtv.org/svn/trunk/checksums/mythtv-0.23-rc3.md5sum">MD5</a>)
+                </li>
+            <li><a href="ftp://ftp.osuosl.org/pub/mythtv/mythplugins-0.23-rc3.tar.bz2">MythPlugins</a>
+                (<a href="http://svn.mythtv.org/svn/trunk/checksums/mythplugins-0.23-rc3.md5sum">MD5</a>)
+                </li>
+            <li><a href="ftp://ftp.osuosl.org/pub/mythtv/myththemes-0.23-rc3.tar.bz2">MythThemes</a>
+                (<a href="http://svn.mythtv.org/svn/trunk/checksums/myththemes-0.23-rc3.md5sum">MD5</a>)
+                </li>
+        </ul></li>
+    <li>Or check the code out from
+        <a href="http://svn.mythtv.org/">subversion</a>.</li>
+</ul>
+
