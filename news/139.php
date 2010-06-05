604edb7e62e5a8f036b0311ec697f54f4ad7294d
Robert McNamara <rmcnamara@mythtv.org>
2010-06-05 19:29:17 +0000

    Add new announcement.
    git-svn-id: file:///usr/src/myth/mythwww/www.mythtv.org@303 474a20e3-f028-0410-92a6-ca0ac9e30e78

---
+++ news/139.php
@@ -0,0 +1,31 @@
+<?php
+    $topic  = 'news';
+    $title  = 'MythUI Patch Competition and .24 Status Update';
+    $author = 'robertm';
+    $date   = strtotime('2010-06-05 19:21:09 UTC');
+?>
+<ul>
+    <li>We wanted to write to a brief announcement on the fast and furious progress
+        being made on MythTV 0.24, and also to recognize the winner of our MythUI
+        patch competition.
+        </li>
+    <li>After considering the submissions made during the 0.22-0.23 development
+        cycle, the winner of our MythUI patch competition is John Poet.  John
+        submitted a massive rework of MythUI button lists that allow the themer
+        to size selected and unselected items differently, allowing the list to
+        dynamically resize and adjust.  This cool feature is already used in many
+        new themes.  John is the winner of a free year's membership to Schedules
+        Direct.
+        </li>
+    <li>Work continues on MythTV 0.24 at a blistering pace.  Over the past several
+        months, features including the HD Audio branch, the MythUI OSD, a complete
+        rework of Program Info handling, unencrypted Blu-ray playback, mass metadata
+        updating in MythVideo, and the foundations of the MythMusic rewrite have been
+        added to trunk.  .24 promises to be a great step forward and we are still on
+        track to bring it to you in the early fall.
+        </li>
+    <li>Congratulations to John and we will wee you all again soon as we solicit
+        testers after the .24 feature freeze.
+        </li>
+</ul>
+
