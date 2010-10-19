e8d752fb50d513f82227ecbc2176e0d105e21c64
Robert McNamara <rmcnamara@mythtv.org>
2010-10-19 00:07:08 +0000

    Add .24-rc1 release announcement.
    git-svn-id: file:///usr/src/myth/mythwww/www.mythtv.org@333 474a20e3-f028-0410-92a6-ca0ac9e30e78

---
+++ news/141.php
@@ -0,0 +1,36 @@
+<?php
+    $topic  = 'announcement';
+    $title  = 'MythTV .24 Release Candidate 1 Now Available!';
+    $author = 'robertm';
+    $date   = strtotime('2010-10-18 23:54:35 UTC');
+?>
+<ul>
+    <li>Just in time for holiday television specials, cold winter nights,
+        and impressing all of your friends and family when they visit,
+        the first Release Candidate for MythTV 0.24 is now available.
+        </li>
+    <li>MythTV .24 will include exciting new features such as a completely rewritten
+        and themeable OSD (On Screen Display), support for 24 bit audio and HD audio formats,
+        initial Blu-ray disc support, experimental support for CrystalHD hardware accelerated
+        video decoding, and many other improvements.  Technical users should feel welcome to
+        make the plunge and help us stamp out the remaining bugs.
+        </li>
+    <li class="bold">Please make sure to read the
+        <a href="http://www.mythtv.org/wiki/Release_Notes_-_0.24">Release Notes</a>
+        before upgrading.</li>
+    <li>You can <a href="ftp://ftp.osuosl.org/pub/mythtv/">download the tarballs</a> now:
+        <ul>
+            <li><a href="ftp://ftp.osuosl.org/pub/mythtv/mythtv-0.24rc1.tar.bz2">MythTV</a>
+                (<a href="http://svn.mythtv.org/svn/trunk/checksums/mythtv-0.24rc1.md5sum">MD5</a>)
+                </li>
+            <li><a href="ftp://ftp.osuosl.org/pub/mythtv/mythplugins-0.24rc1.tar.bz2">MythPlugins</a>
+                (<a href="http://svn.mythtv.org/svn/trunk/checksums/mythplugins-0.24rc1.md5sum">MD5</a>)
+                </li>
+            <li><a href="ftp://ftp.osuosl.org/pub/mythtv/myththemes-0.24rc1.tar.bz2">MythThemes</a>
+                (<a href="http://svn.mythtv.org/svn/trunk/checksums/myththemes-0.24rc1.md5sum">MD5</a>)
+                </li>
+        </ul></li>
+    <li>Or check the code out from
+        <a href="http://svn.mythtv.org/">subversion</a>.</li>
+</ul>
+
