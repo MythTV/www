708fdec0826b0710d3212235812768a410e5a71e
Robert McNamara <rmcnamara@mythtv.org>
2010-11-01 23:10:15 +0000

    .24 RC2 announcement.
    git-svn-id: file:///usr/src/myth/mythwww/www.mythtv.org@336 474a20e3-f028-0410-92a6-ca0ac9e30e78

---
+++ news/142.php
@@ -0,0 +1,32 @@
+<?php
+    $topic  = 'announcement';
+    $title  = 'MythTV .24 Release Candidate 2 Now Available!';
+    $author = 'robertm';
+    $date   = strtotime('2010-10-18 23:54:35 UTC');
+?>
+<ul>
+    <li>The release of MythTV 0.24 is drawing near, and a new Release Candidate is available.
+        </li>
+    <li>Release Candidate 2 brings with it a host of bugfixes and stability improvements,
+        particularly relating to MHEG and DVD playback, fixes for all reported showstopper issues,
+        as well as fixes for a number of minor annoyances.
+        </li>
+    <li class="bold">Please make sure to read the
+        <a href="http://www.mythtv.org/wiki/Release_Notes_-_0.24">Release Notes</a>
+        before upgrading.</li>
+    <li>You can <a href="ftp://ftp.osuosl.org/pub/mythtv/">download the tarballs</a> now:
+        <ul>
+            <li><a href="ftp://ftp.osuosl.org/pub/mythtv/mythtv-0.24rc2.tar.bz2">MythTV</a>
+                (<a href="http://svn.mythtv.org/svn/trunk/checksums/mythtv-0.24rc2.md5sum">MD5</a>)
+                </li>
+            <li><a href="ftp://ftp.osuosl.org/pub/mythtv/mythplugins-0.24rc2.tar.bz2">MythPlugins</a>
+                (<a href="http://svn.mythtv.org/svn/trunk/checksums/mythplugins-0.24rc2.md5sum">MD5</a>)
+                </li>
+            <li><a href="ftp://ftp.osuosl.org/pub/mythtv/myththemes-0.24rc2.tar.bz2">MythThemes</a>
+                (<a href="http://svn.mythtv.org/svn/trunk/checksums/myththemes-0.24rc2.md5sum">MD5</a>)
+                </li>
+        </ul></li>
+    <li>Or check the code out from
+        <a href="http://svn.mythtv.org/">subversion</a>.</li>
+</ul>
+
