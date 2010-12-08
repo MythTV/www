ae9e0cc7286c99499963e880513774788f324082
Chris Petersen <cpetersen@mythtv.org>
2010-12-08 08:39:08 +0000

    News about code.mythtv.org
    git-svn-id: file:///usr/src/myth/mythwww/www.mythtv.org@345 474a20e3-f028-0410-92a6-ca0ac9e30e78

---
+++ news/146.php
@@ -0,0 +1,14 @@
+<?php
+    $topic  = 'news';
+    $title  = 'code.mythtv.org';
+    $author = 'xris';
+    $date   = strtotime('2010-12-08  8:31:32 UTC');
+?>
+<ul>
+    <li>In light of our recent switch to using git, we will be renaming the svn.mythtv.org
+        domain name to code.mythtv.org.</li>
+    <li>Both domains should work at the moment, but we will eventually start redirecting
+        requests to the old svn.mythtv.org domain name.</li>
+    <li>We will also have a few new code-related services running on this host, including
+        experimental copies of Jira and Fisheye.</li>
+</ul>
