dcc45439f24ce42e7fa1574fe5e431ab019ff1d8
Chris Petersen <cpetersen@mythtv.org>
2011-02-12 00:35:47 -0800

    add a note about the rss re-send

--- news/147.php
+++ news/147.php
@@ -8,4 +8,5 @@
     <li>We finished the first phase our server migration tonight by moving web and mail services to their temporary home.</li>
     <li>Expect there to be some delays and slowness in email while DNS and mail servers catch up, especially if you get your email from Yahoo, AT&amp;T, or AOL.</li>
     <li>If you are experiencing any lingering issues by Monday, please speak with xris or beirdo in #mythtv on irc.freenode.net.</li>
+    <li>Also, it appears that something about the new server has confused our RSS feed and made it resend everything.  We apologize for the feed spam this has caused.</li>
 </ul>
