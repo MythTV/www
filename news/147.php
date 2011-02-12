c6536aefac6a841080ad2de587bf9192bee789e5
Chris Petersen <cpetersen@mythtv.org>
2011-02-12 00:34:28 -0800

    New news item, about the mail server migration

---
+++ news/147.php
@@ -0,0 +1,11 @@
+<?php
+    $topic  = 'news';
+    $title  = 'New mailing list server';
+    $author = 'xris';
+    $date   = strtotime('2011-02-12  8:31:10 UTC');
+?>
+<ul>
+    <li>We finished the first phase our server migration tonight by moving web and mail services to their temporary home.</li>
+    <li>Expect there to be some delays and slowness in email while DNS and mail servers catch up, especially if you get your email from Yahoo, AT&amp;T, or AOL.</li>
+    <li>If you are experiencing any lingering issues by Monday, please speak with xris or beirdo in #mythtv on irc.freenode.net.</li>
+</ul>
