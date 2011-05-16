46985a8d9ec510695a61bbae826484a1149e6753
Stuart Morgan <smorgan@mythtv.org>
2011-05-16 12:01:48 +0100

    Post 0.24.1 release announcement

---
+++ news/148.php
@@ -0,0 +1,14 @@
+<?php
+    $topic  = 'announcement';
+    $title  = '0.24.1 Released';
+    $author = 'stuartm';
+    $date   = strtotime('2011-05-16 10:39:38 UTC');
+?>
+
+We've just made our first point release from the 0.24 stable branch which includes all the fixes which have been backported to 0.24 since it was first released in November 2010. Details of the changes since 0.24 can be found in the <a href="http://www.mythtv.org/wiki/Release_Notes_-_0.24.1">Release Notes</a>. I'd like to thank everyone who contributed to this release including those providing detailed bug reports and Matt S. for his great work on the release notes.
+
+This time we are not releasing an updated themes tarball. 0.24.1 includes a fully functional theme downloader within the frontend which allows browsing, downloading and installation of all official and most unofficial themes from a remote server. You should use this feature to obtain updates for your preferred theme from now on.
+
+We'd advise all users currently running 0.24.0 to upgrade, those using the 0.24-fixes branch should already have these changes. If you are using packages and you are in any doubt then please ask the package maintainers which version they are providing.
+
+Work on 0.25 is progressing. We will update you on the changes you can expect and a possible release date in due course.
