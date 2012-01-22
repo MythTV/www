40c0fbc05651114501f332338fc4785f64a4be22
stuartm <smorgan@mythtv.org>
2012-01-22 17:17:29 +0000

    Oops

--- news/149.php
+++ news/149.php
@@ -3,7 +3,5 @@
     $title  = '0.24.2 Released';
     $author = 'stuartm';
     $date   = strtotime('2012-01-22 17:09:57 UTC');
-
-<p>0.24.2, the second release from the stable branch is now available. For a list of fixes made since 0.24.2 see the <a href="http://www.mythtv.org/wiki/Release_Notes_-_0.24.1">Release Notes</a>.</p>
 ?>
-
+<p>0.24.2, the second release from the stable branch is now available. For a list of fixes made since 0.24.2 see the <a href="http://www.mythtv.org/wiki/Release_Notes_-_0.24.1">Release Notes</a>.</p>
