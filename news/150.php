a2e6e8b5f9ecd060a2a6655ae89fce193f3b8bb3
Chris Petersen <cpetersen@mythtv.org>
2012-04-02 21:11:01 -0700

    restore news from old box

---
+++ news/150.php
@@ -0,0 +1,11 @@
+<?php
+    $topic  = 'announcement';
+    $title  = '0.25-beta Released';
+    $author = 'gjhurlbu';
+    $date   = strtotime('2012-03-05 08:23:51 UTC');
+?>
+<p>At long last, we are releasing 0.25-beta.  This is the first stage towards a
+full release of 0.25 (with a target date of April 2).  There have been many
+changes made since the 0.24 releases.  For a list of changes, see the
+<a href="http://www.mythtv.org/wiki/Release_Notes_-_0.25">Release Notes</a>.</p>
+
---
+++ news/151.php
@@ -0,0 +1,8 @@
+<?php
+    $topic  = 'announcement';
+    $title  = '0.25-rc Released';
+    $author = 'gjhurlbu';
+    $date   = strtotime('2012-03-19 07:20:51 UTC');
+?>
+<p>0.25-rc has been released, and our target for full release is Apr 2, 2012</p>
+
