cd8716de25fb815cea4aa4a9542bb0587ab7878b
Gavin Hurlbut <ghurlbut@mythtv.org>
2012-08-06 07:26:05 +0000

    Added 0.26-beta announcement

---
+++ news/155.php
@@ -0,0 +1,16 @@
+<?php
+    $topic  = 'announcement';
+    $title  = '0.26-beta released';
+    $author = 'gjhurlbu';
+    $date   = strtotime('2012-08-06  7:21:18 UTC');
+?>
+
+<p>
+We are happy to announce that 0.26-beta is now ready, although delayed due to
+some deadlock bugs that took longer to debug than expected.
+</p>
+
+<p>
+The release schedule will be updated sometime soon once the beta has had some
+soaking-in testing time, although the string freeze is still set for August 12.
+</p>
