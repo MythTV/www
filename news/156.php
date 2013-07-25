e94beabddde82196ff769728518c99fcc2ab0a75
Stuart Morgan <smorgan@mythtv.org>
2013-07-25 13:51:04 +0100

    0.27 alpha release announcement

---
+++ news/156.php
@@ -0,0 +1,20 @@
+<?php
+    $topic  = 'announcement';
+    $title  = '0.26 Released';
+        $author = 'stuartm';
+        $date   = strtotime('2012-10-03 16:45:37 UTC');
+?>
+<p>0.26 is now available for download. This is a more modest release than those of the recent past with many of the changes being internal reorganisations required to support future developments.</p>
+
+<p>
+Notable changes include:
+<ul>
+<li> HTTP Live Streaming playback and recording </li>
+<li> Conversion of internal times to UTC allowing smoother transitions around daylight savings changes </li>
+<li> Several performance improvements to the UI </li>
+<li> A greater number of translations (nine) are 100% complete for this release with a few more very near to completion. This number should grow by the time of the 0.26.1 point release.  </li>
+<li> The usual bug fixes and incremental improvements </li>
+</ul>
+A more complete list of changes and bug fixes can be found in the <a href="http://www.mythtv.org/wiki/Release_Notes_-_0.26">Release Notes</a>
+</p>
+
---
+++ news/157.php
@@ -0,0 +1,39 @@
+<?php
+    $topic  = 'announcement';
+    $title  = '0.27-alpha released';
+    $author = 'knightr';
+    $date   = strtotime('2013-07-21 12:43:00 UTC');
+?>
+
+<p>We are happy to announce that we have now released v0.27-alpha.</p>
+
+<p>The proposed schedule for the v0.27 release cycle is as follows:
+<br />
+<table>
+<tr>
+<td>July 21th - 0.27-alpha</td>
+<td>Bug fixes only, soft freeze for translatable strings.</td>
+</tr>
+<tr>
+<td>August 4th  - 0.27-beta</td>
+<td>Only critical bug fixes allowed after this date.</td>
+</tr>
+<tr>
+<td>August 11th - 0.27-rc1</td>
+<td> First  Release Candidate, hard freeze for translatable strings.
+        If critical bug fix required, we will cut new rc and wait
+        another week.</td>
+</tr>
+<tr>
+<td>TBD -  rc2-x</td>
+<td>As needed</td>
+</tr>
+<tr>
+<td>TBD - Release</td>
+<td>RC becomes release when no bug fix required for one week.</td>
+</tr>
+</table>
+</p>
+
+<p>As always these dates are subject to change if the resolution of some 
+critical/blocker bugs needed it.</p>
