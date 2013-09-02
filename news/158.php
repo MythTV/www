99bb6c7fee9e39a58ba2aa3a8b529d06dff8f937
stuartm <smorgan@mythtv.org>
2013-09-02 21:40:07 +0000

    Fix typo

--- news/158.php
+++ news/158.php
@@ -7,6 +7,6 @@
 
 <p>The 0.27 release candidate is now available. This release features many bug fixes since the Alpha and Beta releases of July and August. We thank to all those who helped to test 0.27 before now and now invite everyone else to get involved by testing the release candidate. As most of you will be aware, MythTV is a community driven project, entirely supported by the work of volunteers and we need everyone's help to ensure that 0.27 is free of as many bugs as possible before it's final release. So please upgrade to the release candidate and help to make 0.27 the most stable release yet!</p>
 
-<p>Some distributions are already working to the 0.27 available in package form but if you cannot wait, or if a package is not yet available for your platform you can download the source code instead. It's availble in tarball form from our <a href="https://github.com/MythTV/mythtv/archive/fixes/0.27.tar.gz">Github mirror</a> or it may be cloned directly from the git repo using the following command:</p>
+<p>Some distributions are already working to make 0.27 available in package form but if you cannot wait, or if a package is not yet available for your platform you can download the source code instead. It's availble in tarball form from our <a href="https://github.com/MythTV/mythtv/archive/fixes/0.27.tar.gz">Github mirror</a> or it may be cloned directly from the git repo using the following command:</p>
 
 <p><pre>git clone -b fixes/0.27 git://github.com/MythTV/mythtv.git</pre></p>
