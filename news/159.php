fe4b0fb691c78a4be9eb3e1f8d6ee2415418d333
stuartm <smorgan@mythtv.org>
2013-10-01 08:06:27 +0000

    0.27 release announcement

---
+++ news/159.php
@@ -0,0 +1,14 @@
+<?php
+    $topic  = 'announcement';
+    $title  = '0.27 Released';
+    $author = 'stuartm';
+    $date   = strtotime('2013-09-18 21:08:10 UTC');
+?>
+
+<p>0.27 is now available, unless you're one of the many who have been running it since the Release Candidate, in which case it continues to be available! We thought about giving this release a wacky codename, an idea that became even more popular after the news of Android KitKat raised the prospect of a corporate tie-in. At this time sadly our emails to the PR departments of several major breweries have gone unanswered, so the release continues to be called 0.27 and we remain (mostly) sober.</p>
+
+<p>This release saw a determined effort to squash bugs, particularly those found through Static Analysis (Coverity, Cppcheck) and alternate compilers (Clang, Visual Studio et al). This is an on-going effort, but 0.27 has seen issues reported by those applications drop by 95%.</p>
+
+<p>Major new features include support for streaming music stations in mythmusic, Airplay photo sharing and an Airtunes OSD. For our users in the UK and Ireland there is improved Interactive TV support, with iPlayer for Freesat, ConnectedTV for Freeview and Soarview! A full listing can be found in the <a href="http://www.mythtv.org/wiki/Release_Notes_-_0.27">Release Notes</a> (still being updated). Other changes include a number of improvements to the recording scheduling interface to make it simpler for the most common usage, these have proved very popular in feedback from beta testers.</p>
+
+<p>I'd like to remind everyone that 0.25.x has now reached End of Life and 0.26.x is End of Support. Please upgrade to 0.27 to benefit from bug fixes and to receive support.</p>
