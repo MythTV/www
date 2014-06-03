827f0aa78ad89cd3e37e261f0407a986a2a17f3c
Jean-Yves Avenard <jyavenard@mythtv.org>
2014-06-03 10:02:38 +0000

    add 0.27.1 release

---
+++ news/160.php
@@ -0,0 +1,14 @@
+<?php
+    $topic  = 'announcement';
+    $title  = '0.27.1 Released';
+    $author = 'jyavenard';
+    $date   = strtotime('2014-05-27 10:00:00 UTC');
+?>
+
+<p>We are proud to announce MythTV 0.27.1.<br/>0.27.1 is more than your traditional point release: 0.27.1 is our best release ever. While it doesn't have major new user features, it has received over 340 improvements.
+
+<p>Lots have been rewritten under the hood to provide the best user experience ever. In particular, and to make the critics quiet, LiveTV has received an extreme levels of attention.
+
+<p>A big thank you to everyone who made this possible: the devs and all our users who have contributed more than ever via bug reports, bug fixes and testing.
+
+<p>Check the <s href="http://www.mythtv.org/wiki/Release_Notes_-_0.27.1">Release Notes</a> for more details, and how to upgrade your existing system.
