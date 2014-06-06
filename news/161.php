9cb19eaad5d0dbac5dedaf525b7497cf1857d426
Stuart Morgan <smorgan@mythtv.org>
2014-06-06 11:04:02 +0100

    Fix typo

--- news/161.php
+++ news/161.php
@@ -3,11 +3,10 @@
     $title  = 'Our forums are 4 months old today';
     $author = 'stuartm';
     $date   = strtotime('2014-06-05  9:40:19 UTC');
-
+?>
     <p>Four months ago today we launched our official MythTV forums in response to a great demand from our users. To some of you this will be old news, but if you haven't already visited the forum then why not sign up today? It's a great place to share your experiences of MythTV, to find answers to your questions or to help out other members of the community by answering their questions.</p>
 
     <p>Some of you have expressed concern that we will be closing down the mailing lists. I can assure you that this is not the case, the forums are not here to replace the mailing lists but to provide everyone with a choice. Some people prefer forums, other people prefer mailing lists; there are advantages and disadvantages to both.</p>
 
     <p>The forums can be found by clicking on the link at the top right of the page. You can also use this link if that sounds like too much work: <a href="https://forum.mythtv.org/">Official Forums</a></p>
-?>
 
