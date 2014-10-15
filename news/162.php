3653defcedc4e0e114719a19875b60c7a4a00fb0
stuartm <smorgan@mythtv.org>
2014-10-15 19:27:47 +0000

    0.27.4 release announcement

---
+++ news/162.php
@@ -0,0 +1,15 @@
+<?php
+    $topic  = 'announcement';
+    $title  = '0.27.4 Released';
+    $author = 'stuartm';
+    $date   = strtotime('2014-10-15 19:23:24 UTC');
+?>
+
+<p>This is a standard bug fix snapshot of the stable 0.27 branch.</p>
+
+<p>For a complete list of changes see the <a href="https://www.mythtv.org/wiki/Release_Notes_-_0.27.4">Changelog</a></p>
+
+<p>As normal all users are advised to upgrade to benefit from a variety of bug fixes.</p>
+
+<p><b>Important</b>: If you a <b>Schedules Direct</b> user you must upgrade to this release before the 1st November 2014 to continue receiving your guide data without interruption. This is due to a change in the feed address, with the old feed ceasing to work on the previously mentioned date. You need only upgrade, nothing further is required, the data feed is exactly the same it is only the URL which is different.</p>
+
