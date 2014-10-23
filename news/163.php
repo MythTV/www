588571ca8862a49d7d7b4b8842fc71feba9e70e8
stuartm <smorgan@mythtv.org>
2014-10-23 19:21:29 +0000

    Wording.

--- news/163.php
+++ news/163.php
@@ -9,5 +9,5 @@
 
 <p>Unfortunately it seems this news was confused with news of an entirely new JSON feed from Schedules Direct. This feed brings some minor improvements to the Schedules Direct service for MythTV users, and official support for it will come to MythTV through a new XMLTV grabber in due course. An <b>unofficial</b> MythTV grabber was released by Schedules Direct for this new feed but we recommend against it's use. It inserts data directly into the MythTV database, bypassing MythTV's ability to sanity check, correct and act upon information at a single point of insertion. We will be unable to assist users who run this grabber if they encounter bugs or broken features as a result.</p>
 
-<p>So let me repeat, if you upgrade to MythTV 0.27.4 you will continue receive the same Schedules Direct service that you always have with no further action required on your behalf. Official support for the entirely new JSON feed will be added later.</p>
+<p>So, to repeat, if you upgrade to MythTV 0.27.4 you will continue receive the same Schedules Direct service that you always have with no further action required on your behalf. Official support for the entirely new JSON feed will be added later.</p>
 
