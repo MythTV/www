b72e49c0aa6b82775fdcc9b3d16049ae6512b6a3
Stuart Auchterlonie <stuarta@squashedfrog.net>
2016-02-02 14:33:52 +0000

    Fix formatting

--- news/164.php
+++ news/164.php
@@ -3,6 +3,7 @@
     $title  = '0.27.6 Released';
     $author = 'stuarta';
     $date   = strtotime('2016-02-02 12:00:00 UTC');
+?>
 
 <p>This is a standard bug fix snapshot of the stable 0.27 branch.</p>
 
@@ -10,5 +11,4 @@
 
 <p>As normal all users are advised to upgrade to benefit from a variety of bug fixes.</p>
 
-?>
 
