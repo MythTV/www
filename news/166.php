bebeff7410af559afef8bd64083a52774344753b
Stuart Auchterlonie <stuarta@mythtv.org>
2017-02-01 10:08:15 +0000

    rewrite 0.28.1 announcement a bit

--- news/166.php
+++ news/166.php
@@ -5,6 +5,6 @@
     $date   = strtotime('2017-02-01 10:00:27 UTC');
 ?>
 <p>The MythTV Team is pleased to announce the release of MythTV version 0.28.1</p>
-<p>This release is a rollup of all the fixes committed since 0.28 was released</p>
+<p>This is a standard bug fix snapshot of the stable 0.28 branch.</p>
 <p>Full release notes can be found here <a href="https://www.mythtv.org/wiki/Release_Notes_-_0.28.1">Release Notes</a></p>
 
