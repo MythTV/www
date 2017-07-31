3eaba791c6ad2abf609c4b5ed30318dbd4ffadbc
Stuart Auchterlonie <stuarta@mythtv.org>
2017-07-31 13:49:56 +0100

    Release v29.0 announcement

---
+++ news/167.php
@@ -0,0 +1,10 @@
+<?php
+    $topic  = 'announcement';
+    $title  = 'v29.0 Released';
+    $author = 'stuarta';
+    $date   = strtotime('2017-07-31 12:46:07 UTC');
+?>
+<p>The MythTV Team is pleased to announce the release of MythTV version v29.0</p>
+<p>This release is the first release of the new stable branch fixes/29.</p>
+<p>Full release notes can be found here <a href="https://www.mythtv.org/wiki/Release_Notes_-_29">Release Notes</a></p>
+
