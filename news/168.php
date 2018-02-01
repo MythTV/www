437ad5ad677a8c6cea9a660b17059ed20d2f3b96
Stuart Auchterlonie <stuarta@mythtv.org>
2018-02-01 12:30:34 +0000

    Release v29.1, 0.28.2, 0.27.7

---
+++ news/168.php
@@ -0,0 +1,11 @@
+<?php
+    $topic  = 'announcement';
+    $title  = 'Happy Release Day!';
+    $author = 'stuarta';
+    $date   = strtotime('2018-02-01 12:00:00 UTC');
+?>
+<p>The MythTV Team is pleased to announce the release of MythTV version v29.1</p>
+<p>This release includes all the fixes developed over the last 6 months</p>
+<br>
+<p>In addition we have released 0.27.7 and 0.28.2 which represent the last
+planned point releases for 0.27 and 0.28 respectively</p>
