ec497ae69191f3c08c81dbdeb52e9faee526f626
Stuart Auchterlonie <stuarta@mythtv.org>
2019-01-25 17:46:47 +0000

    v30.0 Release announcement

---
+++ news/169.php
@@ -0,0 +1,10 @@
+<?php
+    $topic  = 'announcement';
+    $title  = 'v30.0 Released';
+    $author = 'stuarta';
+    $date   = strtotime('2019-01-14 12:00:00 UTC');
+?>
+<p>The MythTV Team is pleased to announce the release of MythTV version v30.0</p>
+<p>This release is the first release of the new stable branch fixes/30.</p>
+<p>Full release notes can be found here <a href="https://www.mythtv.org/wiki/Release_Notes_-_30">Release Notes</a></p>
+
