5bdedd83ddbcf5fa3afe08e707b9d1f375f97a6e
David Hampton <mythtv@love2code.net>
2025-02-24 11:27:22 -0500

    Release announcement for v35.

---
+++ news/175.php
@@ -0,0 +1,10 @@
+<?php
+    $topic  = 'announcement';
+    $title  = 'v35.0 Released';
+    $author = 'dhampton';
+    $date   = strtotime('2025-02-24 13:32:03 UTC');
+?>
+
+<p>The MythTV Team is pleased to announce the release of MythTV version v35.0</p>
+<p>This release is the first release of the new stable branch fixes/35.</p>
+<p>Full release notes can be found here <a href="https://www.mythtv.org/wiki/Release_Notes_-_35">Release Notes</a></p>
