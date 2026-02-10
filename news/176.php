d35ad6e15b12b8f9c87862bd12178a0b22078fe8
David Hampton <mythtv@love2code.net>
2026-02-09 23:07:14 -0500

    Release announcement for v36.

---
+++ news/176.php
@@ -0,0 +1,10 @@
+<?php
+    $topic  = 'announcement';
+    $title  = 'v36.0 Released';
+    $author = 'dhampton';
+    $date   = strtotime('2026-02-10  4:01:59 UTC');
+?>
+
+<p>The MythTV Team is pleased to announce the release of MythTV version v36.0</p>
+<p>This release is the first release of the new stable branch fixes/36.</p>
+<p>Full release notes can be found here <a href="https://www.mythtv.org/wiki/Release_Notes_-_36">Release Notes</a></p>
